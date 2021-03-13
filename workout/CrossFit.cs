154b007cece11b49f3234e27daabcf98299d911f
Richard Maness <rickrmaness@gmail.com>
2021-03-13 14:31:02 -0500

    Add project files.

---
+++ workout/CrossFit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOutTracker
+{
+    class CrossFit
+    {
+        string crossType;
+        int crossWeight;
+        int crossReps;
+        DateTime;
+    }
+}
---
+++ workout/TypeWeight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOutTracker
+{
+    class TypeWeight
+    {
+        string squats;
+        string deadlift;
+        string benchPress;
+        string militaryPress;
+    }
+}
