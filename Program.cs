ea03ac474c654aa21830b47f1238470cfdec7572
Richard Maness <rickrmaness@gmail.com>
2021-03-18 19:11:42 -0400

    added user input to select CrossFit options and store in a list

--- Program.cs
+++ Program.cs
@@ -3,7 +3,18 @@
 namespace WorkOutTracker
 {
     class Program
-    {   // enum for user input workout lift
+    {
+        //enum for user choice of Crossfit lift
+        public enum TypeCross
+        {
+            Burpees,
+            Thrusters,
+            BoxJumps,
+            Pullup
+        }
+
+
+        // enum for user choice of Weight Lift
         public enum TypeWeight
         {
             BenchPress,
@@ -13,11 +24,43 @@ namespace WorkOutTracker
         }
 
         static void Main(string[] args)
-        {    // User input to choose workout lift and store in variable
+        {
+            //user input to choose crossfit lift and store in variable 
+            Console.WriteLine("Please select lift");
+            string userCross = Console.ReadLine();
+            var cross = (TypeCross)Convert.ToInt32(userCross);
+
+            //user input to choose crossfit weight and store in variable
+            Console.WriteLine("Please select weight for lift");
+            int userCrossWeight = Int32.Parse(Console.ReadLine());
+
+            //user input to choose crossfit sets and store in variable
+            Console.WriteLine("Please enter number of sets");
+            var userCrossSets = Int32.Parse(Console.ReadLine());
+
+            //user input to choose crossfit reps and store in variable 
+            Console.WriteLine("Please enter number of reps");
+            var userCrossReps = Int32.Parse(Console.ReadLine());
+
+            //new list to store userinput as a favorite workout
+            var userCrossWorkout = new CrossFit();
+            userCrossWorkout.CrossType = cross;
+            userCrossWorkout.crossWeight = userCrossWeight;
+            userCrossWorkout.crossSets = userCrossSets;
+            userCrossWorkout.crossReps = userCrossReps;
+
+
+            //display Crossfit workout selection
+            Console.WriteLine();
+            Console.WriteLine("WOD Selection");
+            Console.WriteLine(cross);
+            Console.WriteLine(userCrossSets + userCrossReps);
+            Console.WriteLine(userCrossWeight);
+            
+            // user input to choose weight lift and store in variable
             Console.WriteLine("Please select lift");
             string userLift = Console.ReadLine();
             var lift = (TypeWeight)Convert.ToInt32(userLift);
-
 
             //user input to choose workout weight and store in variable
             Console.WriteLine("Please enter weight for lift");
@@ -31,7 +74,7 @@ namespace WorkOutTracker
             Console.WriteLine("Please enter number of reps");
             int userReps = Int32.Parse(Console.ReadLine());
 
-            //new list to store userinput as a workout 
+            //new list to store userinput as a favorite workout 
             var userWorkout = new Weightlifting();
             userWorkout.LiftType = lift;
             userWorkout.liftWeight = userWeight;
@@ -40,10 +83,10 @@ namespace WorkOutTracker
 
             //dispaly workout selection 
             Console.WriteLine();
+            Console.WriteLine("Workout Selection");
             Console.WriteLine(lift);
             Console.WriteLine(userWeight + "lbs");
             Console.WriteLine(userSets + "x" + userReps);
-       
 
 
         }
--- workout/CrossFit.cs
+++ workout/CrossFit.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static WorkOutTracker.Program;
 
 namespace WorkOutTracker
 {
     class CrossFit
     {
-        public string crossType;
+        public TypeCross CrossType;
         public int crossWeight;
+        public int crossSets;
         public int crossReps;
         DateTime DateTime;
     }
