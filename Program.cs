3debdc99a3ab1f7eff25a5ed73665801c236b463
Richard Maness <rickrmaness@gmail.com>
2021-03-18 19:49:20 -0400

    added user input to select crossfit or weightlifting and added list of lift types to display with lift question

--- Program.cs
+++ Program.cs
@@ -4,20 +4,20 @@ namespace WorkOutTracker
 {
     class Program
     {
+
         //enum for user choice of Crossfit lift
         public enum TypeCross
         {
-            Burpees,
+            Burpees = 1,
             Thrusters,
             BoxJumps,
             Pullup
         }
 
-
         // enum for user choice of Weight Lift
         public enum TypeWeight
         {
-            BenchPress,
+            BenchPress = 1,
             Deadlift,
             MilitaryPress,
             Squats
@@ -25,69 +25,92 @@ namespace WorkOutTracker
 
         static void Main(string[] args)
         {
-            //user input to choose crossfit lift and store in variable 
-            Console.WriteLine("Please select lift");
-            string userCross = Console.ReadLine();
-            var cross = (TypeCross)Convert.ToInt32(userCross);
-
-            //user input to choose crossfit weight and store in variable
-            Console.WriteLine("Please select weight for lift");
-            int userCrossWeight = Int32.Parse(Console.ReadLine());
-
-            //user input to choose crossfit sets and store in variable
-            Console.WriteLine("Please enter number of sets");
-            var userCrossSets = Int32.Parse(Console.ReadLine());
-
-            //user input to choose crossfit reps and store in variable 
-            Console.WriteLine("Please enter number of reps");
-            var userCrossReps = Int32.Parse(Console.ReadLine());
-
-            //new list to store userinput as a favorite workout
-            var userCrossWorkout = new CrossFit();
-            userCrossWorkout.CrossType = cross;
-            userCrossWorkout.crossWeight = userCrossWeight;
-            userCrossWorkout.crossSets = userCrossSets;
-            userCrossWorkout.crossReps = userCrossReps;
+            Console.WriteLine(" What type of workout do you want to do?");
+            Console.WriteLine("CrossFit = 1   Weightlifting = 2");
+            int workoutChoice = Int32.Parse(Console.ReadLine());
+            int userWorkoutChoice = 0;
 
 
-            //display Crossfit workout selection
-            Console.WriteLine();
-            Console.WriteLine("WOD Selection");
-            Console.WriteLine(cross);
-            Console.WriteLine(userCrossSets + userCrossReps);
-            Console.WriteLine(userCrossWeight);
-            
-            // user input to choose weight lift and store in variable
-            Console.WriteLine("Please select lift");
-            string userLift = Console.ReadLine();
-            var lift = (TypeWeight)Convert.ToInt32(userLift);
+            switch (workoutChoice)
+            {
+                case 1:
+                    userWorkoutChoice = 1;
+                    break;
+                case 2:
+                    userWorkoutChoice = 2;
+                    break;
+            }
 
-            //user input to choose workout weight and store in variable
-            Console.WriteLine("Please enter weight for lift");
-            int userWeight = Int32.Parse(Console.ReadLine());
+            if (userWorkoutChoice == 1)
+            {
+                //user input to choose crossfit lift and store in variable 
+                Console.WriteLine("Please select lift");
+                Console.WriteLine("Burpees = 1  Thrusters = 2  Box Jumps = 3 Pullups = 4");
+                string userCross = Console.ReadLine();
+                var cross = (TypeCross)Convert.ToInt32(userCross);
 
-            //user input to choose workout sets and store in variable
-            Console.WriteLine("Please enter number of sets");
-            int userSets = Int32.Parse(Console.ReadLine());
+                //user input to choose crossfit weight and store in variable
+                Console.WriteLine("Please select weight for lift");
+                int userCrossWeight = Int32.Parse(Console.ReadLine());
 
-            //user input to choose workout reps and store in variable 
-            Console.WriteLine("Please enter number of reps");
-            int userReps = Int32.Parse(Console.ReadLine());
+                //user input to choose crossfit sets and store in variable
+                Console.WriteLine("Please enter number of sets");
+                var userCrossSets = Int32.Parse(Console.ReadLine());
 
-            //new list to store userinput as a favorite workout 
-            var userWorkout = new Weightlifting();
-            userWorkout.LiftType = lift;
-            userWorkout.liftWeight = userWeight;
-            userWorkout.liftSets = userSets;
-            userWorkout.liftReps = userReps;
+                //user input to choose crossfit reps and store in variable 
+                Console.WriteLine("Please enter number of reps");
+                var userCrossReps = Int32.Parse(Console.ReadLine());
 
-            //dispaly workout selection 
-            Console.WriteLine();
-            Console.WriteLine("Workout Selection");
-            Console.WriteLine(lift);
-            Console.WriteLine(userWeight + "lbs");
-            Console.WriteLine(userSets + "x" + userReps);
+                //new list to store userinput as a favorite workout
+                var userCrossWorkout = new CrossFit();
+                userCrossWorkout.CrossType = cross;
+                userCrossWorkout.crossWeight = userCrossWeight;
+                userCrossWorkout.crossSets = userCrossSets;
+                userCrossWorkout.crossReps = userCrossReps;
 
+
+                //display Crossfit workout selection
+                Console.WriteLine();
+                Console.WriteLine("WOD Selection");
+                Console.WriteLine(cross);
+                Console.WriteLine(userCrossSets + "x" + userCrossReps);
+                Console.WriteLine(userCrossWeight);
+
+            }
+            else
+            {
+                // user input to choose weight lift and store in variable
+                Console.WriteLine("Please select lift");
+                Console.WriteLine("Bench Press = 1  Deadlift = 2  Military Press = 3  Squats = 4");
+                string userLift = Console.ReadLine();
+                var lift = (TypeWeight)Convert.ToInt32(userLift);
+
+                //user input to choose workout weight and store in variable
+                Console.WriteLine("Please enter weight for lift");
+                int userWeight = Int32.Parse(Console.ReadLine());
+
+                //user input to choose workout sets and store in variable
+                Console.WriteLine("Please enter number of sets");
+                int userSets = Int32.Parse(Console.ReadLine());
+
+                //user input to choose workout reps and store in variable 
+                Console.WriteLine("Please enter number of reps");
+                int userReps = Int32.Parse(Console.ReadLine());
+
+                //new list to store userinput as a favorite workout 
+                var userWorkout = new Weightlifting();
+                userWorkout.LiftType = lift;
+                userWorkout.liftWeight = userWeight;
+                userWorkout.liftSets = userSets;
+                userWorkout.liftReps = userReps;
+
+                //dispaly workout selection 
+                Console.WriteLine();
+                Console.WriteLine("Workout Selection");
+                Console.WriteLine(lift);
+                Console.WriteLine(userWeight + "lbs");
+                Console.WriteLine(userSets + "x" + userReps);
+            }
 
         }
 
